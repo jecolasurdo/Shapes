798b216b84252bfa87b1a701376965e1968035c8
Joe Colasurdo <jecolasurdo@gmail.com>
2014-10-27 22:29:41 -0700

    Wrote spec for the shapebase comparer.

---
+++ Shapes/ShapeCoreSpecs/ShapeBaseSpecs.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Shapes.ShapeCoreSpecs
+{
+    [TestFixture]
+    public class ShapeBaseSpecs
+    {
+        [Test]
+        public void ShapeBase_Comparer_SortsFirstByAreaThenByColor() {
+            Assert.Inconclusive();
+        }
+    }
+}
