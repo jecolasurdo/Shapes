c9f4f86cbe7ef9b7a9f3aeb442dfd6860b22e1c6
Joe Colasurdo <jecolasurdo@gmail.com>
2014-10-28 00:34:18 -0700

    Corrected namespace

--- Shapes/ShapeCoreSpecs/ShapeLogicSpecs/CircleSpecs.cs
+++ Shapes/ShapeCoreSpecs/ShapeLogicSpecs/CircleSpecs.cs
@@ -1,12 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using NUnit.Framework;
 using Shapes.ShapeCore.ShapeLogic;
 
-namespace Shapes.ShapeCoreSpecs.ShapeCoreSpecs
+namespace Shapes.ShapeCoreSpecs.ShapeLogicSpecs
 {
     [TestFixture]
     public class CircleSpecs
--- Shapes/ShapeCoreSpecs/ShapeLogicSpecs/SquareSpecs.cs
+++ Shapes/ShapeCoreSpecs/ShapeLogicSpecs/SquareSpecs.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using NUnit.Framework;
+﻿using NUnit.Framework;
 using Shapes.ShapeCore.ShapeLogic;
 
-namespace Shapes.ShapeCoreSpecs.ShapeCoreSpecs
+namespace Shapes.ShapeCoreSpecs.ShapeLogicSpecs
 {
     [TestFixture]
     public class SquareSpecs
