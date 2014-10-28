0c139b6facff896d60cbfe3d90860879edc22f70
Joe Colasurdo <jecolasurdo@gmail.com>
2014-10-27 21:58:33 -0700

    Added an abstract base shape class that will give all shapes their concept of comparison.

---
+++ Shapes/ShapeCore/ComponentModel/ShapeBase.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shapes.ShapeCore.ComponentModel
+{
+    public abstract class ShapeBase : IShape
+    {
+        public int CompareTo(IShape other) {
+            throw new NotImplementedException();
+        }
+
+        public string ShapeType { get; private set; }
+        public decimal Area { get; private set; }
+        public string Color { get; private set; }
+    }
+}
