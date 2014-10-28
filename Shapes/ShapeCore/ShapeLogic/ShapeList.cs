4358a79343f2132f149c686a2616fc7744867c09
Joe Colasurdo <jecolasurdo@gmail.com>
2014-10-28 00:30:30 -0700

    Stubbed ShapeList class.

--- Shapes/ShapeCore/ShapeLogic/ShapeList.cs
+++ Shapes/ShapeCore/ShapeLogic/ShapeList.cs
@@ -9,14 +9,23 @@ namespace Shapes.ShapeCore.ShapeLogic
 {
     public class ShapeList : ShapeListBase
     {
+        /// <summary>
+        /// Exports the current list in a string with each item separated by line breaks.
+        /// </summary>
         public override string ToString() {
             throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// Accepts a string and attempts to fill the list based on the values found within.
+        /// </summary>
         public override void FillFromString() {
             throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// Returns a subset of this list where the shape names are equal to the specified shape name.
+        /// </summary>
         public override IShapeList GetSubListOfType(string shapeName) {
             throw new NotImplementedException();
         }
