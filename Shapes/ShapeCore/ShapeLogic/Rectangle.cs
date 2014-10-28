6183d7821d851b00be1a00c138d09461b4721710
Joe Colasurdo <jecolasurdo@gmail.com>
2014-10-27 23:31:45 -0700

    Stubbed rectangle.

---
+++ Shapes/ShapeCore/ShapeLogic/Rectangle.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Shapes.ShapeCore.ComponentModel;
+
+namespace Shapes.ShapeCore.ShapeLogic
+{
+    public class Rectangle : ShapeBase
+    {
+        public override string ShapeType { get { return "Rectangle"; } }
+        public override decimal Area { get { throw new NotImplementedException(); } }
+        public override string Color { get { throw new NotImplementedException(); } }
+        public decimal Length { get; private set; }
+        public decimal Width { get; private set; }
+
+        public Rectangle(decimal length, decimal width) {
+            throw new NotImplementedException();
+        }
+    }
+}
