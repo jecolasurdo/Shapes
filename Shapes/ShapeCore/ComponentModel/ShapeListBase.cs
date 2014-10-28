426304c9cd1da6cecb1de35851108f2ade99586d
Joe Colasurdo <jecolasurdo@gmail.com>
2014-10-28 00:26:56 -0700

    Shape list class stubbed.

---
+++ Shapes/ShapeCore/ComponentModel/ShapeListBase.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shapes.ShapeCore.ComponentModel
+{
+    public abstract class ShapeListBase : List<IShape>, IShapeList
+    {
+        public new abstract string ToString();
+        public abstract void FillFromString();
+        public abstract IShapeList GetSubListOfType(string shapeName);
+    }
+}
--- Shapes/ShapeCore/ShapeLogic/ShapeList.cs
+++ Shapes/ShapeCore/ShapeLogic/ShapeList.cs
@@ -3,10 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Shapes.ShapeCore.ComponentModel;
 
 namespace Shapes.ShapeCore.ShapeLogic
 {
-    class ShapeList
+    public class ShapeList : ShapeListBase
     {
+        public override string ToString() {
+            throw new NotImplementedException();
+        }
+
+        public override void FillFromString() {
+            throw new NotImplementedException();
+        }
+
+        public override IShapeList GetSubListOfType(string shapeName) {
+            throw new NotImplementedException();
+        }
     }
 }
