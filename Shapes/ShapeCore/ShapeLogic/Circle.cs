c42156e9a6aed32c8d398ed27103c8bf640984dc
Joe Colasurdo <jecolasurdo@gmail.com>
2014-10-27 22:57:39 -0700

    Stubbed out circle class.

---
+++ Shapes/ShapeCore/ShapeLogic/Circle.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Shapes.ShapeCore.ComponentModel;
+
+namespace Shapes.ShapeCore.ShapeLogic
+{
+    public class Circle : ShapeBase
+    {
+        public override string ShapeType { get { return "Circle"; } }
+        public override decimal Area { get { throw new NotImplementedException(); } }
+        public override string Color { get { throw new NotImplementedException(); } }
+        public double Radius { get; private set; }
+        
+        public Circle(decimal radius) {
+            throw new NotImplementedException();
+        }
+    }
+}
