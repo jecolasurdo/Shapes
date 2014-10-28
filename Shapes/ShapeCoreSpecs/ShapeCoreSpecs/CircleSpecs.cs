143805ab7828816c40f81b475f112094212e5b50
Joe Colasurdo <jecolasurdo@gmail.com>
2014-10-27 23:07:07 -0700

    Stubbed circle specs.

---
+++ Shapes/ShapeCoreSpecs/ShapeCoreSpecs/CircleSpecs.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Shapes.ShapeCoreSpecs.ShapeCoreSpecs
+{
+    [TestFixture]
+    public class CircleSpecs
+    {
+        [Test]
+        public void Circle_Normally_CalculatesAreaCorrectly() {
+            Assert.Inconclusive();
+        }
+
+        [Test]
+        public void Circle_ShapeName_IsCircle() {
+            Assert.Inconclusive();
+        }
+
+        [Test]
+        public void Circle_GivenANegativeRadius_ThrowsArgumentOutOfRangeException() {
+            Assert.Inconclusive();
+        }
+
+        [Test]
+        public void Circle_GivenAnEmptyColor_ThrowsInvalidArgumentException() {
+            Assert.Inconclusive();
+        }
+    }
+}
