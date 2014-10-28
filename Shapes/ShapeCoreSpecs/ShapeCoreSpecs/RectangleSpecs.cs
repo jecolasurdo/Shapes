1b1af5da4f17482a30248c2bfcf66c7ebea1d0f8
Joe Colasurdo <jecolasurdo@gmail.com>
2014-10-27 23:33:23 -0700

    Stubbed rectangle tests.

---
+++ Shapes/ShapeCoreSpecs/ShapeCoreSpecs/RectangleSpecs.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Shapes.ShapeCoreSpecs.ShapeCoreSpecs
+{
+    [TestFixture]
+    public class RectangleSpecs
+    {
+        [Test]
+        public void Rectangle_Name_IsRectangle() {
+            Assert.Inconclusive();
+        }
+
+        [Test]
+        public void Rectangle_Area_CalculatesCorrectly() {
+            Assert.Inconclusive();
+        }
+    }
+}
