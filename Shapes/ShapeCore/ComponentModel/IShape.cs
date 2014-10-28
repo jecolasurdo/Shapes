b406d97b91d755ef5cb956c72c9c58411aab2e4c
Joe Colasurdo <jecolasurdo@gmail.com>
2014-10-27 21:55:45 -0700

    Added interface representing a base shape.

---
+++ Shapes/ShapeCore/ComponentModel/IShape.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shapes.ShapeCore.ComponentModel
+{
+    public interface IShape : IComparable<IShape>
+    {
+        string ShapeType { get; }
+        decimal Area { get; }
+        string Color { get; }
+    }
+}
