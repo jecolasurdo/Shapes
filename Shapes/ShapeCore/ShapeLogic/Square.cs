46ed00e863a8caff058439043f1b9c021cf94333
Joe Colasurdo <jecolasurdo@gmail.com>
2014-10-28 00:02:23 -0700

    Passing square specs.

--- Shapes/ShapeCore/ShapeLogic/Square.cs
+++ Shapes/ShapeCore/ShapeLogic/Square.cs
@@ -9,13 +9,14 @@ namespace Shapes.ShapeCore.ShapeLogic
 {
     public class Square : ShapeBase
     {
-        public override string ShapeType { get { throw new NotImplementedException(); } }
-        public override decimal Area { get { throw new NotImplementedException(); } }
-        public override string Color { get; set; }
+        public override string ShapeType { get { return "Square"; } }
+        public override decimal Area { get { return Length*Length; } }
+        public override sealed string Color { get; set; }
         public decimal Length { get; private set; }
 
         public Square(decimal length, string color) {
-            throw new NotImplementedException();
+            Length = length;
+            Color = color;
         }
     }
 }
