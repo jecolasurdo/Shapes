01f391870c041e7309440da29a23a7546b702ef5
Joe Colasurdo <jecolasurdo@gmail.com>
2014-10-28 02:33:49 -0700

    Add shapesApp project.

---
+++ Shapes/ShapesApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Shapes.ShapesApp
+{
+    class Program
+    {
+        static void Main(string[] args) {
+            var sourcePath = args[0];
+            var shapeFilter = args[1];
+
+        }
+    }
+}
