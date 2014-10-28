d0a4d1783b0ad8c5ef02280b715cf1b1eae371b0
Joe Colasurdo <jecolasurdo@gmail.com>
2014-10-28 00:16:19 -0700

    Built the shape list interface.

---
+++ Shapes/ShapeCore/ComponentModel/IShapeList.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shapes.ShapeCore.ComponentModel
+{
+    public interface IShapeList
+    {
+        string ToString();
+        void FillFromString();
+        IShapeList GetSubListOfType(string shapeName);
+    }
+}
